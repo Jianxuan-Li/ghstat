8e1aa20490eb3d0c383f38881e501ffab450aa5a
Jack Li <liujin834@gmail.com>
2023-11-10 17:03:46 -0500

    feat: using rust image crate

--- src/png.rs
+++ src/png.rs
@@ -1,45 +1,35 @@
-use magick_rust::{magick_wand_genesis, DrawingWand, MagickWand, PixelWand};
-use std::sync::Once;
-
-static START: Once = Once::new();
+use image::{Rgb, RgbImage};
+use imageproc::drawing::{draw_filled_rect_mut, draw_text_mut, text_size};
+use imageproc::rect::Rect;
+use rusttype::{Font, Scale};
+use std::io::Cursor;
 
 pub fn draw_png(num: i32) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    // Initialize MagickWand
-    START.call_once(|| {
-        magick_wand_genesis();
-    });
-    let mut wand = MagickWand::new();
-    let mut pw = PixelWand::new();
+    let mut image = RgbImage::new(100, 16);
 
-    // Create MagickWand
-    wand.new_image(100, 16, &pw).unwrap();
+    let rect = Rect::at(0, 0).of_size(100, 16);
+    let color = Rgb([226u8, 226u8, 226u8]);
+    draw_filled_rect_mut(&mut image, rect, color);
 
-    // Create DrawingWand
-    let mut draw_wand = DrawingWand::new();
+    let font = Vec::from(include_bytes!("DejaVuSans.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
 
-    // set fill color to 226, 226, 226
-    pw.set_color("#E2E2E2").unwrap();
+    let height = 12.4;
+    let scale = Scale {
+        x: height,
+        y: height,
+    };
 
-    // Draw a rectangle (100x16) with the fill color (white)
-    draw_wand.set_fill_color(&pw);
-    draw_wand.draw_rectangle(0.0, 0.0, 100.0, 16.0);
+    let text = format!("Visits {}", num);
+    draw_text_mut(&mut image, Rgb([0u8, 0u8, 0u8]), 12, 2, scale, &font, &text);
+    let (_w, _h) = text_size(scale, &font, &text);
 
-    // Set the font and font size
-    draw_wand.set_font("Arial").unwrap();
-    draw_wand.set_font_size(12.0);
+    let mut buf = Vec::new();
+    let mut cursor = Cursor::new(&mut buf);
 
-    // Set the text color to black
-    pw.set_color("#000").unwrap();
-    draw_wand.set_fill_color(&pw);
+    image
+        .write_to(&mut cursor, image::ImageOutputFormat::Png)
+        .unwrap();
 
-    // Add text "Hello World" at position (0, 0)
-    draw_wand.draw_annotation(12.0, 12.0, &format!("Visits: {}", num)).unwrap();
-
-    // Draw on the MagickWand
-    let _ = wand.draw_image(&draw_wand);
-
-    // Save the image to a buffer
-    let buffer = wand.write_image_blob("PNG").unwrap();
-
-    Ok(buffer)
+    Ok(buf)
 }
