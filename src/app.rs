7230081b9885a30ccdd33d1207680d99ef6bbce9
Jianxuan Li <liujin834@gmail.com>
2024-01-02 22:11:06 -0500

    feat: json response for other entry point

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 use crate::rest::ghstat;
+use crate::rest::estat;
 use thruster::context::typed_hyper_context::TypedHyperContext;
 use thruster::{m, App, HyperRequest};
 
@@ -16,7 +17,8 @@ fn generate_context(request: HyperRequest, _state: &ServerConfig, _path: &str) -
 
 pub async fn app() -> Result<App<HyperRequest, Ctx, ServerConfig>, Box<dyn std::error::Error>> {
     let app = App::<HyperRequest, Ctx, ServerConfig>::create(generate_context, ServerConfig {})
-        .get("/v1/s", m![ghstat]);
+        .get("/v1/s", m![ghstat])
+        .get("/v1/e", m![estat]);
 
     Ok(app)
 }
--- src/rest.rs
+++ src/rest.rs
@@ -50,3 +50,50 @@ pub async fn ghstat(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareR
 
     Ok(context)
 }
+
+#[middleware_fn]
+pub async fn estat(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+    let c: i32;
+
+    {
+        let mut counter = COUNTER.lock().await;
+        counter.increment();
+        c = counter.get_count();
+
+        // get ip address, user agent, and referer from request header
+        let user_agent = context
+            .get_header("user-agent")
+            .pop()
+            .unwrap_or_else(|| "".to_string());
+        let referer = context
+            .get_header("referer")
+            .pop()
+            .unwrap_or_else(|| "".to_string());
+        let ip = context
+            .get_header("x-forwarded-for")
+            .pop()
+            .unwrap_or_else(|| "".to_string());
+        let current_time_str = chrono::Local::now().to_string();
+        let host = context
+            .get_header("host")
+            .pop()
+            .unwrap_or_else(|| "".to_string());
+
+        let log_str = format!(
+            "{}\t{}\t{}\t{}\t{}\t{}",
+            host, current_time_str, ip, user_agent, referer, c
+        );
+        println!("{}", log_str);
+
+        LOG_QUEUE.lock().await.push(log_str);
+    }
+
+    context.remove("server");
+    context.set("Cache-Control", "no-cache, no-store, must-revalidate");
+    
+    // return json with counter
+    let content = format!("{{\"count\":{}}}", &c);
+    context.body(&content);
+
+    Ok(context)
+}
